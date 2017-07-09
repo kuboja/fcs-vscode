eab745e17847cf77f0adde86627689c66fe1cd35
Jan Kubošek <kuboja@outlook.cz>
2017-07-09 20:48:33 +0200

    Remove sayHello command

--- src/extension.ts
+++ src/extension.ts
@@ -11,21 +11,21 @@ export function activate(context: vscode.ExtensionContext) {
 
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "kuboja-fcs" is now active!');
+    //console.log('Congratulations, your extension "kuboja-fcs" is now active!');
 
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
+    //let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
         // The code you place here will be executed every time your command is executed
 
         // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
-    });
+    //    vscode.window.showInformationMessage('Hello World!');
+    //});
 
-    context.subscriptions.push(disposable);
+    //context.subscriptions.push(disposable);
 
-    
+
 
     context.subscriptions.push(
         vscode.languages.registerDocumentSymbolProvider('fcs', new FcsSymbolProvider() )
