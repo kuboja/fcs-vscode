a9b51ead08cae8c5d46c428be26e6ddd6e57c08b
Jan Kubošek <kuboja@outlook.cz>
2018-11-07 19:22:50 +0100

    Add word function

--- src/fcsCompletionItemProvider.ts
+++ src/fcsCompletionItemProvider.ts
@@ -3,7 +3,211 @@
 import * as vscode from "vscode";
 import { FcsGrammar, GrammarType } from "./fcsGrammar";
 import { ExtensionData } from "./extensionData";
+import { Bracket, Brackets } from "./fcsSymbolUtil";
 
+// class CursorWord{
+
+//     private document: vscode.TextDocument;
+//     private position: vscode.Position;
+
+//     /**
+//      *
+//      */
+//     constructor(document: vscode.TextDocument, position: vscode.Position) {
+//         this.document = document;
+//         this.position = position;
+//     }
+
+//     public getWord(){
+//         let prior = WordTools.priorWord(doc, pos);
+
+
+//     }
+
+
+// }
+
+export class WordTools{
+    
+    public static getWordStartPosition(doc: vscode.TextDocument, startLine: number, startChar: number): vscode.Position | undefined {
+
+        let iLine = startLine;
+        let iChar = startChar;
+        let textline = doc.lineAt(iLine).text;
+
+        for (iChar; iChar >= 0; iChar--) {
+            let char = textline[iChar];
+            if (Bracket.isAnyRightBracket(char)) {
+                const newPos = this.findAnyStartingBracket(doc, iLine, iChar, char);
+                if (!newPos) continue;
+                textline = doc.lineAt(newPos.line).text;
+                iLine = newPos.line;
+                iChar = newPos.character;
+            } else if (char == ".") {
+                continue;
+            } else if (!this.isAllowableNameChar(char)) {
+                return new vscode.Position(iLine, iChar + 1);
+            }
+        }
+
+        return new vscode.Position(iLine, 0);
+    }
+
+    public static getWordEndPosition(doc: vscode.TextDocument, startLine: number, startChar: number): vscode.Position | undefined {
+
+        let iLine = startLine;
+        let iChar = startChar;
+        let textline = doc.lineAt(iLine).text;
+
+        for (iChar; iChar <= textline.length; iChar++) {
+            let char = textline[iChar];
+            if (Bracket.isAnyLeftBracket(char)) {
+                const newPos = this.findAnyEndingBracket(doc, iLine, iChar, char);
+                if (!newPos) continue;
+                textline = doc.lineAt(newPos.line).text;
+                iLine = newPos.line;
+                iChar = newPos.character;
+            } else if (char == "."){
+                continue;
+            } else if (!this.isAllowableNameChar(char)) {
+                return new vscode.Position(iLine, iChar - 1);
+            }
+        }
+        
+        return new vscode.Position(iLine, textline.length - 1);
+    }
+
+    public static isAllowableNameChar(char: string){
+        let reg = /^[a-zA-Z_]+$/;
+        return  reg.test(char);
+    }
+
+    public static lookWordStart(doc: vscode.TextDocument, pos: vscode.Position): string | undefined{
+        try {
+            var line: vscode.TextLine = doc.lineAt(pos.line);
+        } catch (error) { return; }
+        
+
+        var text: string = line.text;
+        
+        if (text == undefined || text.length == 0) return;
+
+        for (let curPos = pos.character; curPos > 0; curPos--) {
+            if (text[curPos] == " ") {
+                break;
+            }
+            console.log( text[curPos] + " " + curPos)
+        }
+
+        return;
+    }
+
+    public static findAnyStartingBracket(doc: vscode.TextDocument, startLine: number, startPosition: number, _char: string): vscode.Position | undefined {
+        let char: string;
+        if (_char) {
+            char = _char;
+        } else {
+            char = doc.lineAt(startLine).text[startPosition];
+        }
+
+        let bracket = Bracket.charToBracket(char);
+        if (!bracket) return;
+
+        return this.findStaringBracket(doc, startLine, startPosition, bracket);
+    }
+
+    public static findAnyEndingBracket(doc: vscode.TextDocument, startLine: number, startPosition: number, _char: string): vscode.Position | undefined {
+        let char: string;
+        if (_char) {
+            char = _char;
+        } else {
+            char = doc.lineAt(startLine).text[startPosition];
+        }
+
+        let bracket = Bracket.charToBracket(char);
+        if (!bracket) return;
+
+        return this.findClosingBracket(doc, startLine, startPosition, bracket);
+    }
+
+    public static findStaringBracket(document: vscode.TextDocument, startLine: number, startPosition: number, bracketType: Brackets): vscode.Position | undefined {
+        if (startLine >= document.lineCount)
+            return;
+        
+        const rExp = Bracket.RegExForBoth(bracketType);
+        const leftBracket = Bracket.LeftBracket(bracketType);
+
+        let deep = 0;
+
+        for (let iLine: number = startLine; iLine >= 0; iLine--) {
+
+            let str = document.lineAt(iLine).text;
+
+            if (iLine == startLine) {
+                if (startPosition >= str.length)
+                    return;
+                str = str.substring(0, startPosition + 1);
+            }
+
+            let poss = this.getAllMatches(rExp, str);
+
+            for (let index = poss.length - 1; index >= 0; index--) {
+                const br = poss[index];
+                if (!(deep += str[br.index] === leftBracket ? -1 : 1)) {
+                    return new vscode.Position( iLine, br.index );
+                }
+            }
+        }
+
+        return;
+    }
+
+    public static getAllMatches(regex: RegExp, text: string): RegExpExecArray[] {
+        var res: RegExpExecArray[] = [];
+        var match: RegExpExecArray | null = null;
+
+        if (regex.global) {
+            while (match = regex.exec(text)) {
+                res.push(match);
+            }
+        }
+        else {
+            if (match = regex.exec(text)) {
+                res.push(match);
+            }
+        }
+
+        return res;
+    }
+
+    public static findClosingBracket(document: vscode.TextDocument, startLine: number, startPosition: number, bracketType: Brackets): vscode.Position | undefined {
+        if (startLine >= document.lineCount)
+            return;
+        
+        const rExp = Bracket.RegExForBoth(bracketType);
+        const leftBracket = Bracket.LeftBracket(bracketType);
+
+        const lineCount: number = Math.min(document.lineCount, startLine + 10000);
+
+        rExp.lastIndex = startPosition;
+        
+        let deep = 0;
+        let pos: RegExpExecArray | null;
+
+        for (let iLine: number = startLine; iLine < lineCount; iLine++) {
+
+            let str = document.lineAt(iLine).text;
+
+            while ((pos = rExp.exec(str))) {
+                if (!(deep += str[pos.index] === leftBracket ? 1 : -1)) {
+                    return new vscode.Position(iLine, pos.index);
+                }
+            }
+        }
+
+        return;
+    }
+}
 
 export class FcsCompletionItemProvider implements vscode.CompletionItemProvider {
 
--- test/extension.test.ts
+++ test/extension.test.ts
@@ -8,7 +8,9 @@ import * as assert from 'assert';
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
-//import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import { WordTools } from '../src/fcsCompletionItemProvider';
+import { Brackets } from '../src/fcsSymbolUtil';
 //import * as myExtension from '../src/extension';
 
 // Defines a Mocha test suite to group tests of similar kind together
@@ -19,4 +21,87 @@ suite("Extension Tests", () => {
         assert.equal(-1, [1, 2, 3].indexOf(5));
         assert.equal(-1, [1, 2, 3].indexOf(0));
     });
+
+    test("DocumentWord", async () => {
+        let doc = await vscode.workspace.openTextDocument({content: "asdasda"});
+        assert.equal(doc.getText(), "asdasda");
+    })
+
+    test("Bracket find close", async () => {
+        let doc = await vscode.workspace.openTextDocument({content: "asd asd{saasd{asdasd}dasd}sddfas"});
+        let position = WordTools.findClosingBracket(doc, 0, 6, Brackets.CurlyBracket);
+        assert.equal(position ? position.character : undefined, 25);
+
+        position = WordTools.findClosingBracket(doc, 2, 6, Brackets.CurlyBracket);
+        assert.equal(position ? position.line : undefined, undefined, "number line is out of limit");
+
+        position = WordTools.findClosingBracket(doc, 0, 50, Brackets.CurlyBracket);
+        assert.equal(position ? position.character : undefined, undefined, "end character is out of limit");
+
+        doc = await vscode.workspace.openTextDocument({content: "asd asd{saa\nsd{asd\nasd}dasd}sddfas"});
+        position = WordTools.findClosingBracket(doc, 0, 7, Brackets.CurlyBracket);
+        assert.equal(position ? position.line : undefined, 2, "2nd: line");
+        assert.equal(position ? position.character : undefined, 8, "2nd: end character");
+
+        position = WordTools.findClosingBracket(doc, 1, 2, Brackets.CurlyBracket);
+        assert.equal(position ? position.line : undefined, 2, "3rd: line");
+        assert.equal(position ? position.character : undefined, 3, "3rd: end character");
+    })
+
+    test("Bracket find open", async () => {
+        let doc = await vscode.workspace.openTextDocument({content: "asd asd{saasd{asdasd}dasd}sddfas"});
+        let position = WordTools.findStaringBracket(doc, 0, 25, Brackets.CurlyBracket);
+        assert.equal(position ? position.character : undefined, 7, "1st: start character");
+
+        position = WordTools.findStaringBracket(doc, 2, 6, Brackets.CurlyBracket);
+        assert.equal(position ? position.character : undefined, undefined, "number line is out of limit");
+
+        position = WordTools.findStaringBracket(doc, 0, 50, Brackets.CurlyBracket);
+        assert.equal(position ? position.character : undefined, undefined, "start character is out of limit");
+
+        doc = await vscode.workspace.openTextDocument({content: "asd asd{saa\nsd{asd\nasd}dasd}sddfas"});
+        position = WordTools.findStaringBracket(doc, 2, 8, Brackets.CurlyBracket);
+        assert.equal(position ? position.line : undefined, 0, "2nd: line");
+        assert.equal(position ? position.character : undefined, 7, "2nd: start character");
+
+        position = WordTools.findStaringBracket(doc, 2, 3, Brackets.CurlyBracket);
+        assert.equal(position ? position.line : undefined, 1, "3rd: line");
+        assert.equal(position ? position.character : undefined, 2, "3rd: start character");
+    })
+
+    test("Position of expresion start", async () => {
+        let doc = await vscode.workspace.openTextDocument({content: "asd asd{saa\nsd{asd\nasd}dasd}sddfas"});
+        let position = WordTools.getWordStartPosition(doc, 2, 6);
+        assert.equal(position ? position.line : undefined, 1, "1st: start line");
+        assert.equal(position ? position.character : undefined, 0, "1st: start character");
+       
+        doc = await vscode.workspace.openTextDocument({content: "asd := ads{ \n saa := [1,2,3], \n a := sd{asd,\nasd }, \ndasd}.sddfas"});
+        position = WordTools.getWordStartPosition(doc, 4, 8);
+        assert.equal(position ? position.line : undefined, 0, "2st: start line");
+        assert.equal(position ? position.character : undefined, 7, "2st: start character");
+    })
+
+    test("Position of expresion end", async () => {
+        let doc = await vscode.workspace.openTextDocument({content: "asd asd{saa\nsd{asd\nasd}dasd}sddfas"});
+        let position = WordTools.getWordEndPosition(doc, 0, 4);
+        assert.equal(position ? position.line : undefined, 2, "1st: end line");
+        assert.equal(position ? position.character : undefined, 14, "1st: end character");
+       
+        doc = await vscode.workspace.openTextDocument({content: "asd := ads{ \n saa := [1,2,3], \n a := sd{asd,\nasd }, \ndasd}.sddfas + 1"});
+        position = WordTools.getWordEndPosition(doc, 0, 8);
+        assert.equal(position ? position.line : undefined, 4, "2st: end line");
+        assert.equal(position ? position.character : undefined, 11, "2st: end character");
+    })
+
+
+
+
+    test("Word under cursor", async () => {
+        let doc = await vscode.workspace.openTextDocument({content: "asdasda Fcs.Ahoj.Test"});
+        let pos = new vscode.Position(0, 10);
+
+        let word = WordTools.lookWordStart(doc, pos);
+
+        assert.equal(word, undefined);
+    })
 });
