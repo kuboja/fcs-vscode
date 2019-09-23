cd0c4818782ebbc360f92ec04105010b52dc9932
Jan Kubošek <kuboja@outlook.cz>
2019-09-23 11:41:57 +0200

    Update packages + remove appinsights

--- src/appInsightsClient.ts
+++ src/appInsightsClient.ts
@@ -1,33 +1,35 @@
 "use strict";
 
-import * as appInsights from "applicationinsights";
-import * as os from "os";
+//import {TelemetryClient} from "applicationinsights";
+//import * as os from "os";
 
 
 export class AppInsightsClient {
 
-    private client : appInsights.TelemetryClient;
-    private enableAppInsights: boolean;
+    //private client : TelemetryClient;
+    //private enableAppInsights: boolean;
 
     constructor() {
-        this.enableAppInsights = true;
+        // let appInsights = require("applicationinsights");
 
-        appInsights.setup("6bbe422b-f6e4-46e8-85a7-ca65a09f4157")
-            .setAutoDependencyCorrelation(false)
-            .setAutoCollectRequests(false)
-            .setAutoCollectPerformance(false)
-            .setAutoCollectExceptions(true)
-            .setAutoCollectDependencies(true)
-            .start();
+        // this.enableAppInsights = true;
 
-        this.client = appInsights.defaultClient;
+        // appInsights.setup("6bbe422b-f6e4-46e8-85a7-ca65a09f4157")
+        //     .setAutoDependencyCorrelation(false)
+        //     .setAutoCollectRequests(false)
+        //     .setAutoCollectPerformance(false)
+        //     .setAutoCollectExceptions(true)
+        //     .setAutoCollectDependencies(true)
+        //     .start();
 
-        this.client.context.tags[this.client.context.keys.userId] = os.userInfo().username;
+        // this.client = appInsights.defaultClient;
+
+        // this.client.context.tags[this.client.context.keys.userId] = os.userInfo().username;
     }
 
     public sendEvent(eventName: string): void {
-        if (this.enableAppInsights) {
-            this.client.trackEvent({ name: eventName });
-        }
+      //  if (this.enableAppInsights) {
+      //      this.client.trackEvent({ name: eventName });
+      //  }
     }
 }
